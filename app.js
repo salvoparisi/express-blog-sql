e9cadb938ba2ed422b596dcd59e015a44c999364
salvoparisi <psalvo065@gmail.com>
2024-12-06 15:31:27 +0100

    add destroy db

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use('/', (req, res, next) => {
 */
 
 app.get('/', db.show)
+app.delete('/:id', db.destroy)
 
 
 app.use(notFoundMiddleware);
--- data/db.js
+++ data/db.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     database: 'post_tag',
 });
 
-function show(req, res) {
+const show = (req, res) => {
     const sql = 'SELECT * FROM post_tag.posts';
 
     connection.query(sql, (err, results) => {
@@ -22,4 +22,20 @@ function show(req, res) {
     });
 }
 
-module.exports = { show };
+const destroy = (req, res) => {
+    const id = req.params.id;
+    const sql = 'DELETE FROM posts WHERE id = ?'
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error('Errore durante l\'eliminazione:', err.message);
+            return res.status(500).send('Errore del server');
+        }
+
+        res.send(`Record con ID ${id} eliminato con successo`)
+    })
+}
+module.exports = {
+    show,
+    destroy
+}
