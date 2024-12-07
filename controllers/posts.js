5d749e0f821b9cf18ef1b8596855b8d9efaa030b
salvoparisi <psalvo065@gmail.com>
2024-12-07 02:18:39 +0100

    add showone

--- controllers/posts.js
+++ controllers/posts.js
@@ -14,10 +14,24 @@ const show = (req, res) => {
     });
 }
 
+const showone = (req, res) => {
+    const id = req.params.id;
+    const sql = `SELECT * FROM post_tag.posts WHERE id=${id}`
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error('Errore durante l\'esecuzione della query:', err.message);
+            res.status(500).send('Errore nel server');
+            return;
+        }
+
+        res.json(results);
+    });
+}
 
 const destroy = (req, res) => {
     const id = req.params.id;
-    const sql = `DELETE FROM posts WHERE id = ${id}`
+    const sql = `DELETE FROM post_tag.posts WHERE id=${id}`
 
     connection.query(sql, [id], (err, results) => {
         if (err) {
@@ -30,6 +44,6 @@ const destroy = (req, res) => {
 }
 module.exports = {
     show,
-
+    showone,
     destroy
 }
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const controllers = require('../controllers/posts.js')
 
 router.get('/', controllers.show)
+router.get('/:id', controllers.showone)
 router.delete('/:id', controllers.destroy)
+
 
 module.exports = router
