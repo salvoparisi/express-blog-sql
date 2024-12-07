eb3cfc1acd681b3316ef13b24ba666cb965ea03c
salvoparisi <psalvo065@gmail.com>
2024-12-07 02:29:30 +0100

    add errors

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,13 +1,17 @@
 const connection = require('../data/db.js')
 
 const show = (req, res) => {
-    const sql = 'SELECT * FROM post_tag.posts';
+    const sql = 'SELECT * FROM post_tag.posts'
 
     connection.query(sql, (err, results) => {
         if (err) {
-            console.error('Errore durante l\'esecuzione della query:', err.message);
-            res.status(500).send('Errore nel server');
+            console.error('Errore durante l\'esecuzione della query:', err.message)
+            res.status(500).send('Errore nel server')
             return;
+        }
+        if (results.length == 0) {
+            res.status(404).send('Nessun elemento trovato')
+            return
         }
 
         res.json(results);
@@ -15,7 +19,7 @@ const show = (req, res) => {
 }
 
 const showone = (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
     const sql = `SELECT * FROM post_tag.posts WHERE id=${id}`
 
     connection.query(sql, (err, results) => {
@@ -24,19 +28,26 @@ const showone = (req, res) => {
             res.status(500).send('Errore nel server');
             return;
         }
+        if (results.length == 0) {
+            res.status(404).send(`Nessun elemento trovato con ID ${id}`)
+            return
+        }
 
         res.json(results);
     });
 }
 
 const destroy = (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
     const sql = `DELETE FROM post_tag.posts WHERE id=${id}`
 
     connection.query(sql, [id], (err, results) => {
         if (err) {
-            console.error('Errore durante l\'eliminazione:', err.message);
-            return res.status(500).send('Errore del server');
+            console.error('Errore durante l\'eliminazione:', err.message)
+            return res.status(500).send('Errore del server')
+        }
+        if (results.affectedRows == 0) {
+            return res.status(404).send(`Nessun elemento trovato con ID ${id}`)
         }
 
         res.send(`Record con ID ${id} eliminato con successo`)
